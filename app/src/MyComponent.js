433c624a82569b0a4dd105b1d62f1e70345fe4c2
Amal Sudama <cds.sudama@gmail.com>
2019-02-10 12:56:34 -0500

    remove unused components

--- app/src/MyComponent.js
+++ app/src/MyComponent.js
@@ -2,7 +2,6 @@ import React from "react";
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import {
-  AccountData,
   ContractData,
   ContractForm,
 } from "drizzle-react-components";
@@ -14,80 +13,23 @@ export default ({ accounts, chuckNorrisLore }) => (
     <ToastContainer />
     <div>
       <img src={logo} alt="drizzle-logo" />
-      <h1>Drizzle Examples</h1>
-      <p>Examples of how to get started with Drizzle in various situations.</p>
+      <h1>Drizzle Event Example</h1>
+      <p>Connecting and reacting to Solidity Contract events</p>
     </div>
 
     <div className="section">
+      <h2>dispatch an web request</h2>
       <button onClick={() => chuckNorrisLore() }>Chuck Norris Lore!</button>
     </div>
-    <div className="section">
-      <h2>Active Account</h2>
-      <AccountData accountIndex="0" units="ether" precision="3" />
-    </div>
 
     <div className="section">
-      <h2>SimpleStorage</h2>
-      <p>
-        This shows a simple ContractData component with no arguments, along with
-        a form to set its value.
-      </p>
+      <h2>SimpleStorage with event</h2>
+      <p>Change the value to invoke a contract event</p>
       <p>
         <strong>Stored Value: </strong>
         <ContractData contract="SimpleStorage" method="storedData" />
       </p>
       <ContractForm contract="SimpleStorage" method="set" />
-    </div>
-
-    <div className="section">
-      <h2>TutorialToken</h2>
-      <p>
-        Here we have a form with custom, friendly labels. Also note the token
-        symbol will not display a loading indicator. We've suppressed it with
-        the <code>hideIndicator</code> prop because we know this variable is
-        constant.
-      </p>
-      <p>
-        <strong>Total Supply: </strong>
-        <ContractData
-          contract="TutorialToken"
-          method="totalSupply"
-          methodArgs={[{ from: accounts[0] }]}
-        />{" "}
-        <ContractData contract="TutorialToken" method="symbol" hideIndicator />
-      </p>
-      <p>
-        <strong>My Balance: </strong>
-        <ContractData
-          contract="TutorialToken"
-          method="balanceOf"
-          methodArgs={[accounts[0]]}
-        />
-      </p>
-      <h3>Send Tokens</h3>
-      <ContractForm
-        contract="TutorialToken"
-        method="transfer"
-        labels={["To Address", "Amount to Send"]}
-      />
-    </div>
-    <div className="section">
-      <h2>ComplexStorage</h2>
-      <p>
-        Finally this contract shows data types with additional considerations.
-        Note in the code the strings below are converted from bytes to UTF-8
-        strings and the device data struct is iterated as a list.
-      </p>
-      <p>
-        <strong>String 1: </strong>
-        <ContractData contract="ComplexStorage" method="string1" toUtf8 />
-      </p>
-      <p>
-        <strong>String 2: </strong>
-        <ContractData contract="ComplexStorage" method="string2" toUtf8 />
-      </p>
-      <strong>Single Device Data: </strong>
-      <ContractData contract="ComplexStorage" method="singleDD" />
     </div>
   </div>
 );
